4e42c1c1944a0c5b5c2916eb2979dfc9addc00db
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2020-06-04 13:53:12 -0300

    Disabled button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let input = document.querySelector('#name');
+let button = document.querySelector('.btn');
 
 window.addEventListener('load', () => {
   focusInput();
@@ -7,4 +8,8 @@ window.addEventListener('load', () => {
 
 function focusInput() {
   input.focus();
+}
+
+function disableButton() {
+  button.disabled = true;
 }
