678c98c553e09e4c7852beebdbdf876bf3cccf51
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2020-06-04 13:53:13 -0300

    Adjust data request

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 let input = document.querySelector('#name');
 let button = document.querySelector('.btn');
-let data = [];
+let data = null;
+let adjustedData = null;
 
-async function requisicao() {
+async function request() {
   const response = await fetch(
     'https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo'
   );
   data = await response.json();
+  adjustData();
 }
 
 window.addEventListener('load', () => {
+  request();
   focusInput();
   disableButton();
   inputMonitoring();
@@ -31,5 +34,28 @@ function inputMonitoring() {
   input.addEventListener('keyup', (event) => {
     let buttonContent = event.target.value;
     buttonContent === '' ? disableButton() : enableButton();
+
+    pushButton();
   });
 }
+
+function pushButton() {
+  button.addEventListener('click', () => {
+    filterData();
+    console.log(adjustedData);
+  });
+}
+
+function adjustData() {
+  adjustedData = data.results.map((person) => {
+    const { name, picture, dob, gender } = person;
+    return {
+      name,
+      picture,
+      dob,
+      gender,
+    };
+  });
+}
+
+function filterData() {}
